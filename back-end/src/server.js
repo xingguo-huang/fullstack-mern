b89d918a547d4cc0b0775f1adcaf76ec6272d0fa
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-03 11:19:38 -0800

    Implement article upvote endpoint with initial article data

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,20 +1,21 @@
 import express from 'express';
 
+const articleInfo = [
+  { name: 'learn-node', upvotes: 0 },
+  { name: 'learn-react', upvotes: 0 },
+  { name: 'mongodb', upvotes: 0 },
+]
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/hello', function(req, res) {
-  res.send('Hello from a GET endpoint!');
-});
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const article = articleInfo.find(a => a.name === req.params.name);
+  article.upvotes += 1;
 
-app.get('/hello/:name', function(req, res) {
-  res.send('Hello, ' + req.params.name);
+  res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!');
 });
-
-app.post('/hello', function(req, res) {
-  res.send('Hello, ' + req.body.name + ' from a POST endpoint!');
-})
 
 app.listen(8000, function() {
   console.log('Server is listening on port 8000');
