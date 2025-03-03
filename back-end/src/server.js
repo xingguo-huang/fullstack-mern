9a00ed412ac9ef271aedb38d64c54b6fbc73fe50
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-03 12:24:03 -0800

    Add comments endpoint for articles with dynamic comment storage

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const articleInfo = [
-  { name: 'learn-node', upvotes: 0 },
-  { name: 'learn-react', upvotes: 0 },
-  { name: 'mongodb', upvotes: 0 },
+  { name: 'learn-node', upvotes: 0, comments: [] },
+  { name: 'learn-react', upvotes: 0, comments: [] },
+  { name: 'mongodb', upvotes: 0, comments: [] },
 ]
 
 const app = express();
@@ -14,7 +14,21 @@ app.post('/api/articles/:name/upvote', (req, res) => {
   const article = articleInfo.find(a => a.name === req.params.name);
   article.upvotes += 1;
 
-  res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!');
+  res.json(article);
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articleInfo.find(a => a.name === name);
+
+  article.comments.push({
+    postedBy,
+    text,
+  });
+
+  res.json(article);
 });
 
 app.listen(8000, function() {
