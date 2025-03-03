90ea7f87505da6b69692f281f3e754ae713fef62
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-03 11:07:01 -0800

    Enhance server endpoints with JSON support and dynamic routing

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,9 +2,19 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hello', function(req, res) {
-  res.send('Hello!');
+  res.send('Hello from a GET endpoint!');
 });
+
+app.get('/hello/:name', function(req, res) {
+  res.send('Hello, ' + req.params.name);
+});
+
+app.post('/hello', function(req, res) {
+  res.send('Hello, ' + req.body.name + ' from a POST endpoint!');
+})
 
 app.listen(8000, function() {
   console.log('Server is listening on port 8000');
