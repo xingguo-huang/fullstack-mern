bfe64c9c850c9ab0894281ff8677b0656a333544
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-04 10:51:36 -0800

    Add article loader with Axios to fetch article data dynamically

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,47 +1,48 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import axios from 'axios';
+import './App.css'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import Layout from './Layout';
+import NotFoundPage from './pages/NotFoundPage';
 
-import "./App.css";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ArticlesListPage from "./pages/ArticlesListPage";
-import ArticlePage from "./pages/ArticlePage";
-import Layout from "./Layout";
-import NotFoundPage from "./pages/NotFoundPage";
-
-const routes = [
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/about",
-        element: <AboutPage />,
-      },
-      {
-        path: "/articles",
-        element: <ArticlesListPage />,
-      },
-      {
-        path: "/articles/:name",
-        element: <ArticlePage />,
-      },
-    ],
-  },
-];
+const routes = [{
+  path: '/',
+  element: <Layout />,
+  errorElement: <NotFoundPage />,
+  children: [{
+    path: '/',
+    element: <HomePage />
+  }, {
+    path: '/about',
+    element: <AboutPage />
+  }, {
+    path: '/articles',
+    element: <ArticlesListPage />
+  }, {
+    path: '/articles/:name', // -> /articles/learn-react
+    element: <ArticlePage />,
+    loader: async function() {
+      const response = await axios.get('/api/articles/learn-node');
+      const { upvotes, comments } = response.data;
+      return { upvotes, comments };
+    }
+  }]
+}]
 
 const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
     </>
   );
 }
 
-export default App;
+export default App
