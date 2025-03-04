fd84277a95a862031cf63d607b05079e70e2c71b
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-03 17:31:33 -0800

    Refactor server to use MongoDB for database operations and improve connection handling

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -11,9 +11,9 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/api/articles/:name', async (req, res) => {
-  const { name } = req.params;
+let db;
 
+async function connectToDB() {
   const uri = 'mongodb://127.0.0.1:27017';
 
   const client = new MongoClient(uri, {
@@ -26,18 +26,25 @@ app.get('/api/articles/:name', async (req, res) => {
 
   await client.connect();
 
-  const db = client.db('full-stack-react-db');
+  db = client.db('full-stack-react-db');
+}
 
+app.get('/api/articles/:name', async (req, res) => {
+  const { name } = req.params;
   const article = await db.collection('articles').findOne({ name });
-
   res.json(article);
 });
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-  const article = articleInfo.find(a => a.name === req.params.name);
-  article.upvotes += 1;
+app.post('/api/articles/:name/upvote', async (req, res) => {
+  const { name } = req.params;
 
-  res.json(article);
+  const updatedArticle = await db.collection('articles').findOneAndUpdate({ name }, {
+    $inc: { upvotes: 1 }
+  }, {
+    returnDocument: "after",
+  });
+
+  res.json(updatedArticle);
 });
 
 app.post('/api/articles/:name/comments', (req, res) => {
@@ -54,6 +61,11 @@ app.post('/api/articles/:name/comments', (req, res) => {
   res.json(article);
 });
 
-app.listen(8000, function() {
-  console.log('Server is listening on port 8000');
-});
+async function start() {
+  await connectToDB();
+  app.listen(8000, function() {
+    console.log('Server is listening on port 8000');
+  });
+}
+
+start();
