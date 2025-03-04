b54df869b5a237f8a71125b5aa903cd716c471d1
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-04 14:31:52 -0800

    Implement client-side upvote functionality for articles

--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useParams, useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import CommentsList from '../CommentsList';
@@ -5,14 +6,22 @@ import articles from '../article-content';
 
 export default function ArticlePage() {
   const { name } = useParams();
-  const { upvotes, comments } = useLoaderData();
+  const { upvotes: initialUpvotes, comments } = useLoaderData();
+  const [upvotes, setUpvotes] = useState(initialUpvotes);
 
   const article = articles.find(a => a.name === name);
+
+  async function onUpvoteClicked() {
+    const response = await axios.post('/api/articles/' + name + '/upvote');
+    const updatedArticleData = response.data;
+    setUpvotes(updatedArticleData.upvotes);
+  }
 
   return (
     <>
     <h1>{article.title}</h1>
-    <p>This article has {upvotes} upvotes!</p>
+    <button onClick={onUpvoteClicked}>Upvote</button>
+    <p>This article has {upvotes} upvotes</p>
     {article.content.map(p => <p key={p}>{p}</p>)}
     <CommentsList comments={comments} />
     </>
