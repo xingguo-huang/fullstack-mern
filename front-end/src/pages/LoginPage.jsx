903117b57ac0b0467dcb22aad4646d86c15b219b
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-05 14:15:35 -0800

    Implement login functionality with Firebase authentication

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,5 +1,38 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 export default function LoginPage() {
-    return (
-      <h1>Log In</h1>
-    );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  async function logIn() {
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate('/articles');
+    } catch (e) {
+      setError(e.message);
+    }
   }
+
+  return (
+    <>
+    <h1>Log In</h1>
+    {error && <p>{error}</p>}
+    <input
+      placeholder='Your email address'
+      value={email}
+      onChange={e => setEmail(e.target.value)} />
+    <input
+      placeholder='Your password'
+      type='password'
+      value={password}
+      onChange={e => setPassword(e.target.value)} />
+    <button onClick={logIn}>Log In</button>
+    <Link to='/create-account'>Don't have an account? Create one here</Link>
+    </>
+  );
+}
