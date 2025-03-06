e88111e4bcff591e9c7b50510e6e74b1bb42794d
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-05 16:14:04 -0800

    Add server-side authentication middleware to verify Firebase ID tokens

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -39,6 +39,19 @@ app.get('/api/articles/:name', async (req, res) => {
   res.json(article);
 });
 
+app.use(async function(req, res, next) {
+  const { authtoken } = req.headers;
+
+  if (authtoken) {
+    const user = await admin.auth().verifyIdToken(authtoken);
+    req.user = user;
+  } else {
+    res.sendStatus(400);
+  }
+
+  next();
+});
+
 app.post('/api/articles/:name/upvote', async (req, res) => {
   const { name } = req.params;
 
