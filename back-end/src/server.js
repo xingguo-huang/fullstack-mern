569eee43334779fd101173dd107c35331e0a2c0d
Xingguo Huang <huangxingguo1986@gmail.com>
2025-03-06 16:37:28 -0800

    Configure MongoDB connection for production environment

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -23,7 +23,9 @@ app.use(express.json());
 let db;
 
 async function connectToDB() {
-  const uri = 'mongodb://127.0.0.1:27017';
+  const uri = !process.env.MONGODB_USERNAME
+    ? 'mongodb://127.0.0.1:27017'
+    : `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.zplzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
   const client = new MongoClient(uri, {
     serverApi: {
